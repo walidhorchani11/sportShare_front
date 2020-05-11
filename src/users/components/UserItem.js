db08302c31706147ab1f8f46107e9a88fe8210b5
Horchani Walid <walidhorchani11@gmail.com>
2020-05-11 02:36:55 +0100

    userItem component

---
+++ src/users/components/UserItem.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import './UserItem.css';
+
+const UserItem = ({id, image, name, placeCount}) => {
+  return (
+    <li className="user-item">
+      <div className="user-item__content">
+        <div className="user-item__image">
+          <img src= {image} alt = {name} />
+        </div>
+        <div className="user-item__info">
+          <h2>{name}</h2>
+          <h3>{placeCount} { placeCount === 1 ? 'place' : 'places'}</h3>
+        </div>
+      </div>
+    </li>
+)};
+
+export default UserItem;
