70eae3380819b30ebe6d1e2e992f97a5ed7c4e69
Horchani Walid <walidhorchani11@gmail.com>
2020-05-11 14:33:40 +0100

    styling userItem

--- src/users/components/UserItem.js
+++ src/users/components/UserItem.js
@@ -1,19 +1,23 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 import './UserItem.css';
-
+import Card from '../../shared/components/UIElements/Card';
+import Avatar from '../../shared/components/UIElements/Avatar';
 const UserItem = ({id, image, name, placeCount}) => {
   return (
     <li className="user-item">
-      <div className="user-item__content">
+        <Card className="user-item__content">
+        <Link to = {`/users/${id}/places`}>
         <div className="user-item__image">
-          <img src= {image} alt = {name} />
+          <Avatar image = {image} alt = {name} />
         </div>
         <div className="user-item__info">
           <h2>{name}</h2>
           <h3>{placeCount} { placeCount === 1 ? 'place' : 'places'}</h3>
         </div>
-      </div>
+        </Link>
+        </Card>
     </li>
 )};
 
