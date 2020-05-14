26a55c9ddf96d8504722f8e864bc3de0bf594d42
Horchani Walid <walidhorchani11@gmail.com>
2020-05-14 18:35:21 +0100

    validation

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,7 +1,21 @@
 import React from 'react';
 
+import Input from '../../shared/components/FormElements/Input';
+import { VALIDATOR_REQUIRE } from '../../shared/util/validators';
+import './NewPlace.css';
+
 const NewPlace = () => {
-return <h1>New Place works...</h1>
-}
+  return (
+    <form className="place-form">
+      <Input
+        element="input"
+        type="text"
+        label="Title"
+        validators={[VALIDATOR_REQUIRE()]}
+        errorText="Please enter a valid title."
+      />
+    </form>
+  );
+};
 
 export default NewPlace;
